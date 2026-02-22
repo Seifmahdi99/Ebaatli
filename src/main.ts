72f43d32cbe6e3942e0d6bce1d1c5be2fa740d82
Seif Mahdi <seifeldein173377@bue.edu.eg>
2026-02-22 05:36:32 +0200

    Fix express import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 async function bootstrap() {
   // Create a raw Express instance and register the CSP middleware FIRST,
